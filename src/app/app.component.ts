84d6af708b3c2b6262ddbb566359a7b9d1a320c1
Yohan0412 <Yohan.rosano@hotmail.fr>
2024-12-06 20:07:24 +0100

    exemple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FormControl';
+  username = new FormControl('');
 }
